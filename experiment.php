031e03a9b5a3a3d610f2221eb37718be92a0608d
bri <bcamesi@gmail.com>
2024-04-26 20:39:56 -0400

    php arrays

--- experiment.php
+++ experiment.php
@@ -8,6 +8,7 @@ echo 2 + 2;
 $myname = 'Bri';
 ?>
 
+<!-- Functions -->
 
 <h1>This page is all about <?php echo $myname; ?></h1>
 <?php echo 5 * 5; ?>
@@ -31,3 +32,29 @@ myFirstFunction();
 greet('John', 'blue');
 greet('Jane', 'green');
 ?>
+
+<!-- Arrays -->
+
+<?php 
+
+    $names = array('Brad', 'John', 'Jane', 'Bri');
+
+    // $count = 1;
+
+    // while($count < 101) {
+    //     echo "<li>$count</li>";
+    //     $count++;
+    // }
+
+    $count = 0;
+
+    while($count < count($names)) {
+        echo "<li>Hi, my name is $names[$count].</li>";
+        $count++;
+    }
+
+?>
+
+<p>Hi, my name is <?php echo $names[0]; ?></p>
+
+
